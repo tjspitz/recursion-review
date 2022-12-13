0f089e4f62ed79993d9c99938476d251fa209045
Tom Spitz <tjspitz1986@gmail.com>
2022-12-13 10:00:22 -0600

    Let's try committing this one.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,58 @@
 
 // but you don't so you're going to write it from scratch:
 
+/*
+
+I:any data type
+O:string
+
+Strat:
+need different cases for data types.
+handle arrays and object differently.
+booleans strings numbers add them to result.
+becareful with where weput commas
+*/
+
 var stringifyJSON = function(obj) {
   // your code goes here
+  // number
+  if (typeof obj === 'number') {
+    return '' + obj;
+  }
+  // string
+  if (typeof obj === 'string') {
+    return '"'+ obj + '"';
+  }
+  // boolean
+  if (typeof obj === 'boolean') {
+    return '' + obj;
+  }
+  // function
+  if (typeof obj === 'function') {
+    return undefined;
+  }
+  // undefined
+  if (obj === undefined) {
+    return obj;
+  }
+  // null
+  if (obj === null) {
+    return '' + obj;
+  }
+  // array
+  if (Array.isArray(obj)) {
+    var result = [];
+    obj.forEach(function(item) {
+      result.push(stringifyJSON(item));
+    })
+    return '['+ result.join(',') +']'
+  }
+  // object
+  if (typeof obj) {
+
+  }
 };
+
+
+
+// [1, 2, 3]
