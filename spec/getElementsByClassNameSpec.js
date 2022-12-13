28ae31bc519b1d78530620e51da884314b21a3f0
Tom Spitz <tjspitz1986@gmail.com>
2022-12-13 10:05:10 -0600

    Updated tests to working state.

--- spec/getElementsByClassNameSpec.js
+++ spec/getElementsByClassNameSpec.js
@@ -12,6 +12,7 @@ describe('getElementsByClassName', function() {
 
   it('should match the results of calling the built-in function', function() {
     $('body').addClass('targetClassName');
+
     htmlStrings.forEach(function(htmlString) {
       var $rootElement = $(htmlString);
       $('body').append($rootElement);
@@ -20,7 +21,7 @@ describe('getElementsByClassName', function() {
       var expectedNodeList = document.getElementsByClassName('targetClassName');
       var expectedArray = Array.prototype.slice.apply(expectedNodeList);
       var equality = _.isEqual(result, expectedArray); // why can't we use `===` here?
-      expect(equality).to.equal(FILL_ME_IN);
+      expect(equality).to.equal(true);
 
       $rootElement.remove();
     });
